b9dd4b7cf17f89ea82eb5d79b546b2b76e2bc3f2
Tom <twagner@cecaitra.org.ar>
2024-03-14 09:58:21 -0300

    FEATURE: Modelos registrados en admin
    Se registraron los modelos en admin de api para que se puedan ver,
    modificar y crear desde la vista.

--- backend/vehiculos/admin.py
+++ backend/vehiculos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Vehiculo, TipoVehiculo, FichaVehiculo, Marca
 
-# Register your models here.
+admin.site.register(TipoVehiculo)
+admin.site.register(FichaVehiculo)
+admin.site.register(Marca)
+
+class FichaInLine(admin.StackedInline):
+    model = FichaVehiculo
+    can_delete = False
+    verbose_name = 'Fichas'
+
+@admin.register(Vehiculo)
+class VehiculoAdmin(admin.ModelAdmin):
+    inlines = [FichaInLine]
